136770f6449fb5b4bcb0b3ee73355c1a473490fc
Gabriel <gabriel.araujo.caires@gmail.com>
2025-06-01 10:56:55 -0300

    adicionado e configurado flutter_screenutil

--- lib/app/config/app_config.dart
+++ lib/app/config/app_config.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pokedex/views/home/home_view.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppConfig extends StatelessWidget {
   const AppConfig({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Pokedex PokeApi', home: HomeView());
+    return ScreenUtilInit(
+      designSize: Size(360, 640),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, __) {
+        return MaterialApp(title: 'Pokedex PokeApi', home: HomeView());
+      },
+    );
   }
 }
