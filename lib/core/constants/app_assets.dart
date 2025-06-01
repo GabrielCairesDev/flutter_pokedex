01554bba1e9dd6195f77ff54cfea64f59de4d6e5
Gabriel <gabriel.araujo.caires@gmail.com>
2025-06-01 12:23:27 -0300

    adicionado icone de pesquisa e hashtag

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,3 +1,5 @@
 class AppAssets {
   static const String pokeball = 'assets/svg/pokeball.svg';
+  static const String search = 'assets/svg/search.svg';
+  static const String tag = 'assets/svg/tag.svg';
 }
