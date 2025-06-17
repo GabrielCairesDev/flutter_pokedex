64b52863db5ff1201be7cac8ce4c11e8eab7f3c9
Gabriel Caires <gabriel.araujo.caires@gmail.com>
2025-06-17 18:21:10 -0300

    feature(GridViewWidget): aprimorado e componetizado

---
+++ lib/shared/widgets/grid_Item_pokemon.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/app/config/app_colors.dart';
+import 'package:flutter_pokedex/app/config/app_typography.dart';
+import 'package:flutter_pokedex/models/pokemon_model.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class GridItemPokemon extends StatelessWidget {
+  const GridItemPokemon({super.key, required this.pokemon});
+
+  final PokemonModel pokemon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.r),
+        color: AppColors.grayScaleWhite,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            offset: const Offset(0, 1),
+            blurRadius: 1.5,
+            spreadRadius: 1,
+          ),
+        ],
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 64.h,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 44.h,
+              decoration: BoxDecoration(
+                color: AppColors.grayScaleBackground,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(8.r)),
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 8.w, vertical: 4.h),
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                      '#${pokemon.id}',
+                      style: AppTypography().caption.copyWith(
+                        color: AppColors.grayScaleMedium,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 72.h,
+                    width: 72.w,
+                    child: pokemon.imageUrl.isNotEmpty
+                        ? Image.network(pokemon.imageUrl)
+                        : Container(color: Colors.blue),
+                  ),
+                  Text(
+                    pokemon.name,
+                    style: AppTypography().body3.copyWith(
+                      color: AppColors.grayScaleDark,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
