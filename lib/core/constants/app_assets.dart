85df2ab462bfd47fddc3d71bca14071fa2ed01fb
Gabriel Caires <gabriel.araujo.caires@gmail.com>
2025-06-17 12:27:46 -0300

    feature(AppAssets): adicionar construtor privado

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,4 +1,5 @@
 class AppAssets {
+  AppAssets._();
   static const String pokeball = 'assets/svg/pokeball.svg';
   static const String search = 'assets/svg/search.svg';
   static const String tag = 'assets/svg/tag.svg';
