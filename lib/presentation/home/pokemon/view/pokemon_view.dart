1e2f6b52cbb3b0598369c6e22aa5711a11d20656
Gabriel Caires <gabriel.araujo.caires@gmail.com>
2025-06-17 21:09:08 -0300

    feature(PokemonView): adicionado view

---
+++ lib/presentation/home/pokemon/view/pokemon_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PokemonView extends StatefulWidget {
+  const PokemonView({super.key});
+
+  @override
+  State<PokemonView> createState() => _PokemonViewState();
+}
+
+class _PokemonViewState extends State<PokemonView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
