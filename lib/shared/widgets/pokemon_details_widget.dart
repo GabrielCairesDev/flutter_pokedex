eb22a7f77ccd8b04003a7e6d367899579ec9204a
Gabriel Caires <gabriel.araujo.caires@gmail.com>
2025-06-18 09:30:00 -0300

    featur(PokemonDetailsWidget): widget com os detalhes do pokemon

---
+++ lib/shared/widgets/pokemon_details_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/app/config/app_colors.dart';
+import 'package:flutter_pokedex/models/pokemon_model.dart';
+import 'package:flutter_pokedex/shared/widgets/pokemon_type_widget.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class PokemonDetailsWidget extends StatelessWidget {
+  const PokemonDetailsWidget({super.key, required this.pokemon});
+
+  final PokemonModel pokemon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 190.h, right: 4.w, left: 4.w, bottom: 4.w),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.r),
+          color: AppColors.grayScaleWhite,
+        ),
+        child: Column(
+          children: [
+            SizedBox(height: 48.h),
+            PokemonTypeWidget(types: pokemon.types),
+          ],
+        ),
+      ),
+    );
+  }
+}
