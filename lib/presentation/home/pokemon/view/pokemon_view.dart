290bb21ed4ec4377fb18299e9353e78f33c6ed09
Gabriel Caires <gabriel.araujo.caires@gmail.com>
2025-06-18 09:30:41 -0300

    featur(pokemonview): updates

--- lib/presentation/home/pokemon/view/pokemon_view.dart
+++ lib/presentation/home/pokemon/view/pokemon_view.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/presentation/home/pokemon/viewModel/pokemon_view_model.dart';
+import 'package:flutter_pokedex/shared/widgets/app_bar_widget.dart';
+import 'package:flutter_pokedex/shared/widgets/big_pokeball_widget.dart';
+import 'package:flutter_pokedex/shared/widgets/cached_image_with_loader.dart';
+import 'package:flutter_pokedex/shared/widgets/pokemon_details_widget.dart';
+import 'package:flutter_pokedex/shared/widgets/scaffold_widget.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PokemonView extends StatefulWidget {
   const PokemonView({super.key});
@@ -7,9 +14,40 @@ class PokemonView extends StatefulWidget {
   State<PokemonView> createState() => _PokemonViewState();
 }
 
+final vm = PokemonViewModel();
+
 class _PokemonViewState extends State<PokemonView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListenableBuilder(
+      listenable: vm,
+      builder: (context, _) {
+        return ScaffoldWidget(
+          backgroundColor: vm.pokemonSelected.color,
+          body: Column(
+            children: [
+              Expanded(
+                child: Stack(
+                  children: [
+                    PokemonDetailsWidget(pokemon: vm.pokemonSelected),
+                    BigPokeball(),
+                    Column(
+                      children: [
+                        AppBarWidget(),
+                        CachedImageWithLoader(
+                          imageUrl: vm.pokemonSelected.imageUrl,
+                          width: 200.w,
+                          height: 200.w,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
--- lib/presentation/home/pokemon/viewModel/pokemon_view_model.dart
+++ lib/presentation/home/pokemon/viewModel/pokemon_view_model.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/models/pokemon_model.dart';
+import 'package:flutter_pokedex/presentation/home/viewmodel/home_view_model.dart';
+import 'package:get_it/get_it.dart';
 
-class PokemonViewModel extends ChangeNotifier {}
+class PokemonViewModel extends ChangeNotifier {
+  HomeViewModel get _homeViewModel => GetIt.I.get<HomeViewModel>();
+
+  PokemonModel get pokemonSelected => _homeViewModel.pokemonSelected!;
+}
