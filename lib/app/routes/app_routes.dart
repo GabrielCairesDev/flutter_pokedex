6827913a0c1b33a1e9421db25cf3076fb9a6cc39
Gabriel Caires <gabriel.araujo.caires@gmail.com>
2025-06-17 21:09:59 -0300

    feature(AppRoutes): adicionado rota

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/presentation/home/pokemon/view/pokemon_view.dart';
 import 'package:flutter_pokedex/presentation/home/view/home_view.dart';
 
 class AppRoutes {
+  AppRoutes._();
   static const String home = '/home';
+  static const String pokemon = '/pokemon';
 
   static Map<String, WidgetBuilder> get routes => {
     home: (context) => HomeView(),
+    pokemon: (context) => const PokemonView(),
   };
 }
