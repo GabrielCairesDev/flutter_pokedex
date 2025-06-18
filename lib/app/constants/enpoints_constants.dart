d5be29cf23076a8906f6737cbdd269792c1b59b2
Gabriel Caires <gabriel.araujo.caires@gmail.com>
2025-06-18 10:31:00 -0300

    removido arquivos

--- lib/app/constants/enpoints_constants.dart
+++
@@ -1,9 +0,0 @@
-class EnpointsConstants {
-  EnpointsConstants._();
-
-  static const String pokemonsSpeciesCount =
-      'https://pokeapi.co/api/v2/pokemon-species/?limit=0';
-
-  static String pokemonDetails(int id) =>
-      'https://pokeapi.co/api/v2/pokemon/$id';
-}
