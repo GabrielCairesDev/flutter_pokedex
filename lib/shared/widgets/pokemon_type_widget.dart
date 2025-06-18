0fe10fe9a7ef7161d8b93b5ec0d846268717ee88
Gabriel Caires <gabriel.araujo.caires@gmail.com>
2025-06-18 09:29:46 -0300

    featur(PokemonTypeWidget): widget com o tipo do pokemon

---
+++ lib/shared/widgets/pokemon_type_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/app/config/app_colors.dart';
+import 'package:flutter_pokedex/app/config/app_typography.dart';
+import 'package:flutter_pokedex/core/constants/pokemon_type_color.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class PokemonTypeWidget extends StatelessWidget {
+  const PokemonTypeWidget({super.key, required this.types});
+
+  final List<String> types;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: types
+          .map(
+            (type) => Padding(
+              padding: EdgeInsets.only(right: 24.w),
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 2.h),
+                decoration: BoxDecoration(
+                  color: PokemonTypeColor.getColor(type),
+                  borderRadius: BorderRadius.circular(10.r),
+                ),
+                child: Text(
+                  type,
+                  style: AppTypography().subtitle3.copyWith(
+                    color: AppColors.grayScaleWhite,
+                  ),
+                ),
+              ),
+            ),
+          )
+          .toList(),
+    );
+  }
+}
