4c3e52ebd8e8056431df103c9af24072f9e0e4e7
Gabriel Caires <gabriel.araujo.caires@gmail.com>
2025-06-18 09:27:26 -0300

    feature(AppAssets): adicionado arrow_left.svg

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -3,4 +3,5 @@ class AppAssets {
   static const String pokeball = 'assets/svg/pokeball.svg';
   static const String search = 'assets/svg/search.svg';
   static const String tag = 'assets/svg/tag.svg';
+  static const String arrowLeft = 'assets/svg/arrow_left.svg';
 }
